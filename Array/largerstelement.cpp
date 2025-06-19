7bdf92023da8aee7806e67232c74e7075489e16f
Kavin1103 <srikavin1103@gmail.com>
2025-06-19 21:22:02 +0530

    Learning Array 1

--- Array/largerstelement.cpp
+++ Array/largerstelement.cpp
@@ -4,23 +4,17 @@ using namespace std;
 
 int main() {
     int n;
-    cout << "Enter number of elements: ";
     cin >> n;
-
     vector<int> arr(n);
-
-    cout << "Enter " << n << " elements:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    for(int i=0; i<=n-1; i++){
+        cin>> arr[i];
     }
-
     int largest = arr[0];
-    for (int j = 1; j < n; j++) {
-        if (arr[j] > largest) {
+    for(int j=0; j<=n-1; j++){
+        if(arr[j]>largest){
             largest = arr[j];
         }
     }
-
-    cout << "The largest is: " << largest << endl;
+    cout<<"The largest is"<< largest;
     return 0;
 }
